4c1cb246c9c7407014913659ffa67347481f5ef4
abhik345 <abhikdas345@gmail.com>
2024-06-02 12:48:10 +0530

    react query tank

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PostListing from "./components/PostListing";
+function App() {
+  return (
+    <>
+      <h2 className="title">My Posts</h2>
+      <PostListing/>
+    </>
+  );
+}
+
+export default App;
